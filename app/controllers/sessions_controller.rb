009398dea73b5fb62183977cbfd48a04f41f85ec
Phenix2014 <phenix.cai@gmail.com>
2015-09-16 06:49:27 +0000

    add simple login/logout interface

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #user login and redirect to user page
+      log_in user
+      redirect_to user
+    else
+      # generate an error message
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
