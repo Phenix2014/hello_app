13a6e84a919331138dc824c862c0ec54c7ceae95
Phenix2014 <phenix.cai@gmail.com>
2015-09-16 12:09:28 +0000

    use cookie to keep the login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -41,6 +41,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
   end
   
+  
+
   test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
@@ -54,11 +56,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
-  end  
-  
-  
+  end
+
 end
